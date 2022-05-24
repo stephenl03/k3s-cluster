a95d93f199a942e48a25429140c34885d8d1c655
Stephen <stephenl@me.com>
2022-05-24 18:36:40 -0500

    add kr to firewall exclusions, remove au

--- provision/terraform/cloudflare/firewall_rules.tf
+++ provision/terraform/cloudflare/firewall_rules.tf
@@ -4,13 +4,13 @@
 
 resource "cloudflare_filter" "countries" {
   zone_id     = lookup(data.cloudflare_zones.domain.zones[0], "id")
-  description = "Expression to block all countries except US, CA and AU"
-  expression  = "(ip.geoip.country ne \"US\" and ip.geoip.country ne \"CA\" and ip.geoip.country ne \"AU\")"
+  description = "Expression to block all countries except US, CA and KR"
+  expression  = "(ip.geoip.country ne \"US\" and ip.geoip.country ne \"CA\" and ip.geoip.country ne \"KR\")"
 }
 
 resource "cloudflare_firewall_rule" "countries" {
   zone_id     = lookup(data.cloudflare_zones.domain.zones[0], "id")
-  description = "Firewall rule to block all countries except US, CA and AU"
+  description = "Firewall rule to block all countries except US, CA and KR"
   filter_id   = cloudflare_filter.countries.id
   action      = "block"
 }
