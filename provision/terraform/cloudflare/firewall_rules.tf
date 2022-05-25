780745ee17e4dbec8d8a9778ec359f584b198612
Stephen <stephenl@me.com>
2022-05-24 19:45:44 -0500

    addd mx to firewall exclusions, remove ca

--- provision/terraform/cloudflare/firewall_rules.tf
+++ provision/terraform/cloudflare/firewall_rules.tf
@@ -4,13 +4,13 @@
 
 resource "cloudflare_filter" "countries" {
   zone_id     = lookup(data.cloudflare_zones.domain.zones[0], "id")
-  description = "Expression to block all countries except US, CA and KR"
-  expression  = "(ip.geoip.country ne \"US\" and ip.geoip.country ne \"CA\" and ip.geoip.country ne \"KR\")"
+  description = "Expression to block all countries except US, MX and KR"
+  expression  = "(ip.geoip.country ne \"US\" and ip.geoip.country ne \"MX\" and ip.geoip.country ne \"KR\")"
 }
 
 resource "cloudflare_firewall_rule" "countries" {
   zone_id     = lookup(data.cloudflare_zones.domain.zones[0], "id")
-  description = "Firewall rule to block all countries except US, CA and KR"
+  description = "Firewall rule to block all countries except US, MX and KR"
   filter_id   = cloudflare_filter.countries.id
   action      = "block"
 }
